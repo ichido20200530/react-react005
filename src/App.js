6f89000412d4439a19b1724f896ae9570f593c6e
ichido <ichido@live.jp>
2020-06-04 08:16:27 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  msgStyle = {
+    fontSize:"24pt",
+    color:"#900",
+    margin:"20px 0px",
+    padding: "5px",
+    borderBottom:"2px solid #900"
+  }
+  btnStyle = {
+    fontSize:"20pt",
+    padding:"0px 10px"
+  }
+
+  constructor(props){
+    super(props);
+    this.state = {
+      msg: 'Hello',
+    };
+    setInterval(() => {
+      this.setState((state) => ({
+        msg: state.msg + "!"
+      }));
+    }, 5000);
+  }
+
+  render(){
+    return <div>
+      <h1>React</h1>
+      <p style={this.msgStyle}>{this.state.msg}</p>
+      <p style={this.msgStyle}>{this.props.msg}</p>
+    </div>;
+  }
 }
 
 export default App;
